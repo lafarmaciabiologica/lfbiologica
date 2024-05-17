cd252fd1a7dc87725a54e3d4755736689fdba830
Over Garcia <gerencia@innovatecsa.com>
2024-05-17 15:59:48 -0500

    Otros módulos

---
+++ l10n_co_account_tax/models/__init__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2022 INNOVATECSA S.A.S. (http://www.innovatecsa.com)
+#
+##############################################################################
+
+from . import account_tax
+from . import account_move
+from . import account_journal
+from . import sale_order
+from . import purchase_order
+
+
+
